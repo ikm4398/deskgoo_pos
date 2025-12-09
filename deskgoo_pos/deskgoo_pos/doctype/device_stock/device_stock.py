0536e8ac5a6059f17085d3aa192ad3edf07f6f73
Indra Kumar Mehta <ikm4398@gmail.com>
2025-12-09 09:01:35 +0000

    .

--- deskgoo_pos/deskgoo_pos/doctype/device_stock/device_stock.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Indra @Deskgoo and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class DeviceStock(Document):
-	pass
